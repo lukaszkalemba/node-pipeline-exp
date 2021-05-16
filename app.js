2e79bd65ac1fc87055ca6b4ebca4d541c2297245
Lukasz Kalemba <lukasz.kalemba@appstract.software>
2021-05-16 13:06:34 +0200

    Update get route second time

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('lorem ipsum'));
+app.get('/', (req, res) => res.send('lorem ipsum dolor sit amet'));
 
 const PORT = 8080;
 
